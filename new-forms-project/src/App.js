3648e21770857710a9ecdc8b4ef89994baa87a59
Alec Venida <alecvenida@gmail.com>
2022-06-16 15:45:05 -0700

    form skeleton

--- new-forms-project/src/App.js
+++ new-forms-project/src/App.js
@@ -1,6 +1,50 @@
 function App() {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log()
+  }
+
   return (
-    <h1>Hello from App</h1>
+    <div>
+    <h1>Form</h1>
+      <form handleSubmit = {handleSubmit}>
+        <div>
+            <label>Name</label>
+            <input type='text' id='name'/>
+        </div>
+        <div>
+            <label>Email</label>
+            <input type='text' id='email'/>
+        </div>
+        <div>
+            <label>Phone Number</label>
+            <input type='text' id='phone'/>
+        </div>
+        <div>
+            <label>Phone Type</label>
+            <select id='phoneType'>
+              <option disabled>Select a phone type</option>
+              <option>Home</option>
+              <option>Work</option>
+              <option>Mobile</option>
+            </select>
+        </div>
+        <div>
+            <label>Staff</label>
+            <input type='radio' id='instructor'/>Instructor
+            <input type='radio' id='student'/>Student
+        </div>
+        <div>
+            <label>Bio</label>
+            <textarea id='bio'/>
+        </div>
+        <div>
+            <label>Signup for email notifications</label>
+            <input type="checkbox"/>
+        </div>
+        <button type='submit'>Submit</button>
+      </form>
+    </div>
   );
 }
 
