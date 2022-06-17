f2ee3ecd3cf70141b9e7534e77cd49f56e864167
Attiya <adarensburg@live.com>
2022-06-17 01:50:30 -0500

    latest mods for react form

--- new-forms-project/src/App.js
+++ new-forms-project/src/App.js
@@ -5,16 +5,20 @@ function App() {
   const[name, setName]=useState('');
   const[email, setEmail]=useState('');
   const[phone, setPhone]=useState('');
-  const[phoneType, setPhoneType]=useState('');
+  const[phoneType, setPhoneType]=useState('Select a phone type');
   const[staff, setStaff]=useState('');
   const[bio, setBio]=useState('');
   const[notification, setNotification]=useState(false);
   const [validationErrors, setValidationErrors] = useState([]);
 
+
+
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    if(validationErrors.length !== 0) alert('Cannot submit with errors!');
+    const phoneValidator = new RegExp('[0-9]{3}-[0-9]{3}-[0-9]{4}');
+
+    if(phone.match(phoneValidator) && phoneType === 'Select a phone type') alert('select a phone number type');
 
     console.log('Name: ', name);
     console.log('Email: ', email);
@@ -27,61 +31,74 @@ function App() {
     setName('');
     setEmail('');
     setPhone('');
-    setPhoneType('');
+    setPhoneType('Select a phone type');
     setBio('');
-    setNotification('');
+    setNotification(false);
 
 
   }
 
   useEffect(() => {
+    const emailValidator = new RegExp('[A-Za-z0-9]+@[a-z]+.[a-z]{2,3}');
+    const phoneValidator = new RegExp('[0-9]{3}-[0-9]{3}-[0-9]{4}');
     const errors = [];
+
     if(name.length === 0) errors.push('please provide a name');
-    if(!email.includes('@')) errors.push('please provide a valid email address');
+    if(!email.match(emailValidator)) errors.push('please provide a valid email address');
+    if(!phone.match(phoneValidator)) errors.push('please provide a valid phone number with the following format: xxx-xxx-xxxx');
+    if(staff.length === 0) errors.push('select a staff role');
+
     setValidationErrors(errors);
-
-  },[name,email]);
-
+  },[name,email,bio]);
 
   return (
     <div>
-    <h1>Form</h1>
-      <form onSubmit = {handleSubmit}>
+      <h1>Form</h1>
+        {validationErrors.length !== 0 && (
         <div>
-            <label>Name</label>
-            <input type='text' id='name' value={name} onChange={e => setName(e.target.value)}/>
+          <ul>
+            {validationErrors.map(error => {
+              return <li key={error}>{error}</li>;
+            })}
+          </ul>
         </div>
-        <div>
-            <label>Email</label>
-            <input type='text' id='email' value={email} onChange={e => setEmail(e.target.value)}/>
-        </div>
-        <div>
-            <label>Phone Number</label>
-            <input type='text' id='phone' value={phone} onChange={e => setPhone(e.target.value)}/>
-        </div>
-        <div>
-            <label>Phone Type</label>
-            <select id='phoneType' value={phoneType} onChange={e => setPhoneType(e.target.value)}>
-              <option disabled>Select a phone type</option>
-              <option>Home</option>
-              <option>Work</option>
-              <option>Mobile</option>
-            </select>
-        </div>
-        <div>
-            <label>Staff</label>
-            <input type='radio' id='instructor' value={'Instructor'} onChange={(e) => setStaff(e.target.value)} checked={staff === 'Instructor' ? 'checked':''}/>Instructor
-            <input type='radio' id='student' value={'Student'} onChange={e => setStaff(e.target.value)} checked={staff === 'Student' ? 'checked':''}/>Student
-        </div>
-        <div>
-            <label>Bio</label>
-            <textarea id='bio' value={bio} onChange={e => setBio(e.target.value)}/>
-        </div>
-        <div>
-            <label>Signup for email notifications</label>
-            <input type="checkbox" value={notification} onChange={() => setNotification(!notification)}/>
-        </div>
-        <button type='submit'>Submit</button>
+      )}
+        <form onSubmit = {handleSubmit}>
+          <div>
+              <label>Name</label>
+              <input type='text' id='name' value={name} onChange={e => setName(e.target.value)}/>
+          </div>
+          <div>
+              <label>Email</label>
+              <input type='text' id='email' value={email} onChange={e => setEmail(e.target.value)}/>
+          </div>
+          <div>
+              <label>Phone Number</label>
+              <input type='text' id='phone' value={phone} onChange={e => setPhone(e.target.value)}/>
+          </div>
+          <div>
+              <label>Phone Type</label>
+              <select id='phoneType' value={phoneType} onChange={e => setPhoneType(e.target.value)}>
+                <option disabled>Select a phone type</option>
+                <option>Home</option>
+                <option>Work</option>
+                <option>Mobile</option>
+              </select>
+          </div>
+          <div>
+              <label>Staff</label>
+              <input type='radio' id='instructor' value={'Instructor'} onChange={(e) => setStaff(e.target.value)} checked={staff === 'Instructor' ? 'checked':''}/>Instructor
+              <input type='radio' id='student' value={'Student'} onChange={e => setStaff(e.target.value)} checked={staff === 'Student' ? 'checked':''}/>Student
+          </div>
+          <div>
+              <label>Bio</label>
+              <textarea id='bio' value={bio} onChange={e => setBio(e.target.value)} maxLength={280} placeholder={'max length 280 characters'}/>
+          </div>
+          <div>
+              <label>Signup for email notifications</label>
+              <input type="checkbox" value={notification} onChange={() => setNotification(!notification)}/>
+          </div>
+          <button type='submit'>Submit</button>
       </form>
     </div>
   );
